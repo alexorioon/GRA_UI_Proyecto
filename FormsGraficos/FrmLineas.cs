83c4e0476416289439826d4ce315bd596f27d114
Alejandro Arroyo Lozano <alex02alex87@gmail.com>
2025-04-15 18:17:29 -0600

    Avance en "GenerarRectangulos()"

---
+++ FormsGraficos/FrmLineas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsGraficos
+{
+    public partial class FrmLineas : Form
+    {
+        public FrmLineas()
+        {
+            InitializeComponent();
+        }
+        Graphics graphics;
+        private void ptbDibujo_Paint(object sender, PaintEventArgs e)
+        {
+            graphics = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 3);
+            graphics.DrawLine(pen, new Point(50, 50), new Point(100, 50));
+            graphics.DrawLine(pen, new Point(100, 50), new Point(100, 100));
+            graphics.DrawLine(pen, new Point(100, 100), new Point(50, 100));
+            graphics.DrawLine(pen, new Point(50, 100), new Point(50, 50));
+
+        }
+    }
+}
---
+++ FormsGraficos/FrmLineasEscalar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsGraficos
+{
+    public partial class FrmLineasEscalar : Form
+    {
+        public FrmLineasEscalar()
+        {
+            InitializeComponent();
+        }
+
+        Graphics graphics;
+        int ex = 1, ey = 1;
+        private void ptbDibujo_Paint(object sender, PaintEventArgs e)
+        {
+            graphics = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 3);
+
+            Rectangle rectangle = new Rectangle(50, 50, 50 + ex, 50 + ey);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+
+        private void FrmLineasEscalar_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                    ex++;
+                    ey++;
+                    ptbDibujo.Refresh();
+                    break;
+
+                case Keys.Down:
+                    if (ex > 1)
+                    {
+                        ex--;
+                        ey--;
+                        ptbDibujo.Refresh();
+
+                    }
+                    break;
+            }
+        }
+    }
+}
